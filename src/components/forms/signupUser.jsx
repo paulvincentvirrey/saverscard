ef5ca20866f3bb212118662ed947b50f8c9c4289
PAUL VIRREY <paul.v.m.virrey@accenture.com>
2019-09-09 22:10:14 +0800

    reset active step on signup user

--- src/components/forms/signupUser.jsx
+++ src/components/forms/signupUser.jsx
@@ -103,7 +103,7 @@ class SignupUser extends Component {
     this.state = {
       values: { subscription: 0, paymentMethod: "Promo Code" },
       errors: {},
-      activeStep: 2
+      activeStep: 0
     };
   }
 
